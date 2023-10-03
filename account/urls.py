d28c1661825776e75fd23bb0b6e5988f1372bacf
Pavel <kunpanpavel@gmail.com>
2023-10-03 22:24:12 +0300

    add register form

--- account/urls.py
+++ account/urls.py
@@ -2,9 +2,8 @@ from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 
-
 urlpatterns = [
-    #path('login/', views.user_login, name='login'),
+    # path('login/', views.user_login, name='login'),
     # url login and logout
     # path('login/', auth_views.LoginView.as_view(), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
@@ -23,4 +22,5 @@ urlpatterns = [
 
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
 ]
