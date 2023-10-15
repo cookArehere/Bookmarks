879e8163b1e4cd03b89965719ced4801eb513b74
Pavel <kunpanpavel@gmail.com>
2023-10-15 22:02:08 +0300

    add verification email for used

--- account/forms.py
+++ account/forms.py
@@ -24,11 +24,24 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError('Password don\'t match.')
         return cd['password2']
 
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
+
 
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+
+    def clean_email(self):
+        date = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id).filter(email=date)
+        if qs.exists():
+            raise forms.ValidationError('Email already in use.')
+        return date
 
 
 class ProfileEditForm(forms.ModelForm):
